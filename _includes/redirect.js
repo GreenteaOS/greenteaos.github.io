7f98288664d7191b980dc4bf4eb7055c106c669c
k00lagin <k00lagin@gmail.com>
2016-08-09 18:34:06 +0500

    redirection improvements

---
+++ _includes/redirect.js
@@ -0,0 +1,9 @@
+{% unless page.lang %}
+<script>
+    var lang = ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.userLanguage || window.navigator.language || window.navigator.systemLanguage).substr(0, 2).toLowerCase();
+    available = { {{ include.available }} };
+    if (available[lang]) {
+        location.pathname = lang + location.pathname;
+    }
+</script>
+{% endunless %}
