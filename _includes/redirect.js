6de7dda626954ce6ac9771053240cf201bd6c4a4
k00lagin <k00lagin@gmail.com>
2016-08-20 16:06:45 +0500

    global settings for available langs

--- _includes/redirect.js
+++ _includes/redirect.js
@@ -1,7 +1,7 @@
 {% unless page.lang %}
 <script>
     var lang = ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.userLanguage || window.navigator.language || window.navigator.systemLanguage).substr(0, 2).toLowerCase();
-    available = { {{ include.available }} };
+    available = { {{ site.available }} };
     if (available[lang]) {
         location.pathname = lang + location.pathname;
     }
