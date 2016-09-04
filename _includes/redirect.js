627e4dd6b493177c3c72e4d5418dffe47788ff5a
k00lagin <k00lagin@gmail.com>
2016-09-04 18:18:08 +0500

    change lang storing

--- _includes/redirect.js
+++ _includes/redirect.js
@@ -2,7 +2,7 @@
 <script>
     var lang = ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.userLanguage || window.navigator.language || window.navigator.systemLanguage).substr(0, 2).toLowerCase();
     available = { {{ site.available }} };
-    if (available[lang]) {
+    if (available.indexOf(lang) >= 0) {
         location.pathname = lang + location.pathname;
     }
 </script>
