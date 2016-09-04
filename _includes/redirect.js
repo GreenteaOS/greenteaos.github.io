7e65400d04d697a566829381a28e5e71c6c49daa
k00lagin <k00lagin@gmail.com>
2016-09-04 19:15:29 +0500

    Update redirect.js

--- _includes/redirect.js
+++ _includes/redirect.js
@@ -1,6 +1,6 @@
 {% unless page.lang %}
 <script>
-    var lang = ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.userLanguage || window.navigator.language || window.navigator.systemLanguage).substr(0, 2).toLowerCase();
+    var lang = (localStorage && localStorage.preferredLanguage) || ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.userLanguage || window.navigator.language || window.navigator.systemLanguage).substr(0, 2).toLowerCase();
     available = { {{ site.available }} };
     if (available.indexOf(lang) >= 0) {
         location.pathname = lang + location.pathname;
