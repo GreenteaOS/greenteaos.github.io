3c63a544cf2349c164f08946be07fb6eb76ef6ef
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2019-10-25 18:53:26 -0700

    Disable useless redirect to avoid 404

--- _includes/redirect.js
+++ _includes/redirect.js
@@ -3,7 +3,9 @@
     var lang = (localStorage && localStorage.preferredLanguage) || ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.userLanguage || window.navigator.language || window.navigator.systemLanguage).substr(0, 2).toLowerCase();
     available = "{{ site.available }}";
     if (available.indexOf(lang) >= 0) {
-        location.pathname = lang + location.pathname;
+        if (false) {
+            location.pathname = lang + location.pathname;
+        }
     }
 </script>
 {% endunless %}
